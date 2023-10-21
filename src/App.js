d7da211575678ffe07140a06a43552940d4df12d
priyanka <yadavpriyanka9628@gmail.com>
2023-10-21 12:35:21 +0530

    added toast

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Layout from './components/Layout/Layout';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
-    <Layout />
+    <>
+      <Layout />
+      <ToastContainer />
+    </>
   );
 }
 
--- src/components/UI/product-card/ProductCard.js
+++ src/components/UI/product-card/ProductCard.js
@@ -6,12 +6,28 @@ import productImg from "../../../assets/images/product_2.1.jpg";
 import { useDispatch } from 'react-redux'
 
 import { cartActions } from "../../../store/shopping-cart/cartSlice"
-
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ProductCard = (props) => {
     const { id, title, image01, price } = props.item;
     const dispatch = useDispatch();
+    const cart = useSelector((state) => state.cart.cartItems);
+    console.log(cart)
+
+
+    const deleteItem = (event) => {
+        dispatch(cartActions.deleteItem(id));
+        event.stopPropagation();
+        toast.success(`${title} removed from cart`, {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: true,
+        });
+    };
+
 
     const addToCart = () => {
         dispatch(
@@ -21,7 +37,13 @@ const ProductCard = (props) => {
                 image01,
                 price
             })
+
         );
+        toast.success(`${title} added to cart`, {
+            position: "top-right",
+            autoClose: 2000, // Auto close the toast after 2 seconds
+            hideProgressBar: true,
+        });
     };
 
     return (
@@ -34,9 +56,19 @@ const ProductCard = (props) => {
             </div>
             <div className="d-flex flex-column align-items-center justify-content-between">
                 <span className="product__price mb-2">{price} € </span>
-                <button className="addTOCART__btn" onClick={addToCart}>
-                    Add to Cart
-                </button>
+                {
+                    cart.some((p) => p.id == id) ? <button className="addTOCART__btn" onClick={event => deleteItem(event)}>
+                        Remove item
+
+
+                    </button> :
+                        <button className="addTOCART__btn" onClick={addToCart}>
+                            Add to Cart
+
+
+                        </button>
+                }
+
             </div>
         </div>
     );
